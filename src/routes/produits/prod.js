d8241572d922d14a4bb5cf0b14f9ac5362c6d237
Jossuc4 <larryjossuc4@gmail.com>
2024-04-26 01:16:22 +0300

    parse POST data

--- src/routes/produits/prod.js
+++ src/routes/produits/prod.js
@@ -16,23 +16,47 @@ prodRouter.get('/:categorie', (req, res) => {
     })
 })
 
+
 prodRouter.post('/commande', (req, res) => {
-    // const {id_produit,qte_produit} = req.body
-    addProdCommand(req.body, (response) => {
+
+    const parameter = JSON.parse(req.body.data)
+
+    // // const {id_produit,qte_produit} = req.body
+    // addProdCommand(req.body, (response) => {
+    //     console.log(response)
+    // })
+
+    addProdCommand(parameter, (response) => {
         console.log(response)
     })
-    // reduceProduct({ qte_minus: parseInt(qte_produit), id: id_produit }, (response) => {
-    reduceProduct(req.body, (response) => {
+
+    // // reduceProduct({ qte_minus: parseInt(qte_produit), id: id_produit }, (response) => {
+    // reduceProduct(req.body, (response) => {
+    //     let { affectedRows } = response
+    //     res.status(200).end(JSON.stringify(affectedRows))
+    // })
+
+    reduceProduct(parameter, (response) => {
         let { affectedRows } = response
         res.status(200).end(JSON.stringify(affectedRows))
     })
+
 })
 
 prodRouter.post('/payement', (req, res) => {
-    PayOrderedCart(req.body, (response)=>{
+
+    const parameter = JSON.parse(req.body.data)
+
+    // PayOrderedCart(req.body, (response) => {
+    //     let { affectedRows } = response
+    //     res.status(200).end(JSON.stringify(affectedRows))
+    // })
+
+    PayOrderedCart(parameter, (response) => {
         let { affectedRows } = response
         res.status(200).end(JSON.stringify(affectedRows))
     })
+
 })
 
 module.exports = prodRouter;
